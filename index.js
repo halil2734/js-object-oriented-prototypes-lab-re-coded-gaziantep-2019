d655db86a4c739b8011c768e36bc816e8866d241
Halil güler <h.guler3717@gmail.com>
2019-07-18 13:30:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,18 +6,18 @@
    this.prototype.veto = function(){
      return 'No, I must disagree'
    }
-   this.approve = function(){
+   this.prototype.approve = function(){
      return 'You can do that!'
    }
-   this.doCharity = function(){
+   this.prototype.doCharity = function(){
      return "I like to help people."
    }
 
-   this.releasePressStatement = function(){
+   this.prototype.releasePressStatement = function(){
      return "You will see great things from Scuber."
    }
 
-   this.sayHi = function(){
+   this.prototype.sayHi = function(){
      return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
    }
  }
